14d814922b6bb59f2f56f8b05fec9b393736cdf9
unknown <sakhiimam3411@gmail.com>
2022-10-11 06:05:53 -0700

    changes

--- src/contexapi/TableContex.jsx
+++ src/contexapi/TableContex.jsx
@@ -1,17 +1,25 @@
 import { useReducer } from "react"
 import {createContext} from "react"
 import tableReducer ,{initialState} from "../reducer/tableReducer"
+
 const Api="http://localhost:3004/table"
-const TableContex=createContext(initialState)
+
+
+const TableContex=createContext()
 
 export const TableProvider=({children})=>{
       const [state,dispatch]=useReducer(tableReducer,initialState);
     
-      const  GetAlldata=async(data)=>{
+      const  GetAlldata=async(Api)=>{
           try {
-            
+                const data=axios.get(Api)
+                dispatch({
+                    payload:{
+                        data:data
+                    }
+                })
           } catch (error) {
-            
+              console.log(error)
           }
       }
 }
